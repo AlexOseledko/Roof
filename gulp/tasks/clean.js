58d187b0ecea240af9e947b4b3eda163c89595e7
Alexey Oseledko <AlexeyOseledko@gmail.com>
2022-09-07 14:57:00 +0300

    clean

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -3,7 +3,7 @@ const del = require('del');
 // Полностью удаляем папку dist
 
 module.exports = function clean(cb) {
-  return del(['dist', 'WALK-IN-SHOWER', 'T1', 'T2', 'T3', 'T4']).then(() => {
+  return del(['dist']).then(() => {
     cb()
   })
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp');
 const script = require('./gulp/tasks/scripts');
 const fonts = require('./gulp/tasks/fonts');
-const copySubDomain = require('./gulp/tasks/copySubDomain');
 const videos = require('./gulp/tasks/videos');
 const vendors = require('./gulp/tasks/vendorsJS');
 const imageMinify = require('./gulp/tasks/imageMinify');
@@ -12,7 +11,7 @@ const spriteSVG = require('./gulp/tasks/spriteSVG');
 const serve = require('./gulp/tasks/serve');
 const spritePNG = require('./gulp/tasks/spritePNG');
 
-const dev = gulp.parallel(pug2html, script, vendors, styles, imageMinify, spriteSVG, spritePNG, fonts, videos, copySubDomain);
+const dev = gulp.parallel(pug2html, script, vendors, styles, imageMinify, spriteSVG, spritePNG, fonts, videos);
 
 exports.default = gulp.series(
   clean,
